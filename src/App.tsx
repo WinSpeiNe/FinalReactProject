5f9099c3224c3616c62c2cbea455bd0c0d28d904
WinSpeiNe <izik1747@gmail.com>
2020-03-25 13:57:26 +0200

    remove some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
-import {useDispatch, useSelector, connect} from "react-redux";
-import {increment} from "./actions";
-import {decrement} from "./actions";
-import {singIn} from "./actions";
 
-interface stateA {
-    counter: number,
-    isLogged: boolean
-}
-
-function App() {
-
-    const tempCounter = (state: stateA) => state.counter;
-    const tempLogged = (state: stateA) => state.isLogged;
-
-    const counter = useSelector(tempCounter);
-    const isLogged = useSelector(tempLogged);
-
-    const dispatch = useDispatch();
-
+const App: FC = () => {
     return (
         <div className="App">
-            <h1>📌 Counter: {counter} 📌</h1>
-            <button onClick={() => dispatch(increment())}>+</button>
-            <button onClick={() => dispatch(decrement())}>-</button>
-            <button onClick={()=>dispatch(singIn())}> Log In / Sing Out</button>
-            {isLogged && <h3> You Logged In!  </h3>}
+            First Project
         </div>
     );
-}
+};
 
 export default App;
--- src/reducers/isLogged.ts
+++
@@ -1,9 +0,0 @@
-const loggerReducer = (state = false, action: any) => {
-    if (action.type === 'SIGN_IN') {
-        return !state;
-    } else {
-        return state;
-    }
-};
-
-export default loggerReducer;
